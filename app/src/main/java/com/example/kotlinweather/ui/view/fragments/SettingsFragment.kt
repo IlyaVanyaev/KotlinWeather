ddaff6938be2b77f1cfa9170eff15ca1cef56b7d
Ilya <IL4Van@yandex.ru>
2023-10-15 23:21:13 +0300

    added delete animation (*)

--- app/src/main/java/com/example/kotlinweather/ui/view/fragments/SettingsFragment.kt
+++ app/src/main/java/com/example/kotlinweather/ui/view/fragments/SettingsFragment.kt
@@ -9,6 +9,7 @@ import android.view.View
 import android.view.ViewGroup
 import android.widget.Toast
 import androidx.lifecycle.ViewModelProvider
+import com.airbnb.lottie.LottieDrawable
 import com.example.kotlinweather.R
 import com.example.kotlinweather.databinding.FragmentSettingsBinding
 import com.example.kotlinweather.ui.viewmodel.WeatherViewModel
@@ -43,10 +44,11 @@ class SettingsFragment : Fragment() {
 
         binding.settingsBackground.setImageResource(R.drawable._0492524)
         binding.settingsTitle.text = "Settings"
-        binding.settingsDelete.setImageResource(R.drawable.delete)
+        //binding.settingsDelete.setImageResource(R.drawable.delete)
         binding.settingsTextToDelete.text = "Delete all weather info"
 
         binding.settingsDelete.setOnClickListener {
+            playAnimation(0.0f, 0.3f, 1.0f, 0, LottieDrawable.RESTART)
             deleteAllFromDataBase()
         }
 
@@ -55,14 +57,27 @@ class SettingsFragment : Fragment() {
     private fun deleteAllFromDataBase(){
         val dialog = AlertDialog.Builder(activity)
         dialog.setPositiveButton("Yeah buddy!") {_,_ ->
+            playAnimation(0.3f, 1.0f, 1.0f, 0, LottieDrawable.RESTART)
             weatherViewModel.deleteAll()
             Toast.makeText(activity, "Database cleared", Toast.LENGTH_SHORT).show()
         }
-        dialog.setNegativeButton("Negative"){_,_ ->}
+        dialog.setNegativeButton("Negative"){_,_ ->
+            playAnimation(0.0f, 0.3f, -1.0f, 0, LottieDrawable.RESTART)
+        }
         dialog.setTitle("Delete")
         dialog.setMessage("Are you sure you want to DELETE all weather information from the Database?")
         dialog.setIcon(R.drawable.delete)
         dialog.create().show()
+    }
+
+
+
+    private fun playAnimation(min:Float, max: Float, speed: Float, repeat: Int, mode:Int){
+        binding.settingsDelete.setMinAndMaxProgress(min, max)
+        binding.settingsDelete.repeatCount = repeat
+        binding.settingsDelete.repeatMode = mode
+        binding.settingsDelete.speed = speed
+        binding.settingsDelete.playAnimation()
     }
 
     companion object {
