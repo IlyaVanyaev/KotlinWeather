6b2d95ce0ad355677ef9a99ab8cede46fef7b3b2
Ilya <IL4Van@yandex.ru>
2023-10-23 10:21:10 +0300

    added animated icon in InfoFragment (*)

--- app/src/main/java/com/example/kotlinweather/ui/view/fragments/InfoFragment.kt
+++ app/src/main/java/com/example/kotlinweather/ui/view/fragments/InfoFragment.kt
@@ -6,13 +6,20 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.activityViewModels
+import androidx.lifecycle.lifecycleScope
+import com.airbnb.lottie.LottieDrawable
 import com.example.kotlinweather.R
 import com.example.kotlinweather.databinding.FragmentInfoBinding
+import com.example.kotlinweather.ui.viewmodel.MainFragmentViewModel
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
 
 
 class InfoFragment : Fragment() {
 
     private lateinit var binding: FragmentInfoBinding
+    private val viewModel : MainFragmentViewModel by activityViewModels()
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -27,11 +34,17 @@ class InfoFragment : Fragment() {
     ): View {
         binding = FragmentInfoBinding.inflate(inflater, container, false)
 
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
         binding.infoBackground.setImageResource(R.drawable._0492524)
         binding.infoTitle.text = "KotlinWeather"
         binding.infoDescription.text = "This app was made by MIREA student, Ilya Vanyaev, IKBO-07-21"
 
-        return binding.root
+        lifecycleScope.launch(Dispatchers.IO) { viewModel.playAnimation(binding.infoIcon, 0.0f, 1.0f, 1.0f, LottieDrawable.INFINITE, LottieDrawable.INFINITE) }
     }
 
 
