b6b826c60290495988abe1128ad1aea122b812e9
Ilya <IL4Van@yandex.ru>
2023-10-29 18:44:24 +0300

    added Card in InfoFragment using Compose (7)

--- app/src/main/java/com/example/kotlinweather/ui/view/fragments/InfoFragment.kt
+++ app/src/main/java/com/example/kotlinweather/ui/view/fragments/InfoFragment.kt
@@ -6,14 +6,44 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.compose.foundation.BorderStroke
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.border
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxHeight
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.offset
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Shapes
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.Shape
+import androidx.compose.ui.platform.ComposeView
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import androidx.fragment.app.activityViewModels
 import androidx.lifecycle.lifecycleScope
 import com.airbnb.lottie.LottieDrawable
-import com.example.kotlinweather.R
 import com.example.kotlinweather.databinding.FragmentInfoBinding
 import com.example.kotlinweather.ui.viewmodel.MainFragmentViewModel
 import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.FlowPreview
 import kotlinx.coroutines.launch
+import android.R
 
 
 class InfoFragment : Fragment() {
@@ -31,20 +61,24 @@ class InfoFragment : Fragment() {
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View {
-        binding = FragmentInfoBinding.inflate(inflater, container, false)
-
-        return binding.root
+    ): View{
+        return ComposeView(requireContext()).apply {
+            setContent{
+                infoCard()
+            }
+        }
     }
 
-    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
-        super.onViewCreated(view, savedInstanceState)
 
-        //binding.infoBackground.setImageResource(R.drawable._0492524)
-        binding.infoTitle.text = "KotlinWeather"
-        binding.infoDescription.text = "This app was made by MIREA student, Ilya Vanyaev, IKBO-07-21"
+    @Composable
+    fun infoCard(){
+        Column (modifier = Modifier.fillMaxWidth().fillMaxHeight(), horizontalAlignment = Alignment.CenterHorizontally, verticalArrangement = Arrangement.Center){
+            Card (modifier = Modifier.width(300.dp).height(450.dp).offset(0.dp, (-45).dp), shape = RoundedCornerShape(25.dp), colors = CardDefaults.cardColors(Color(0xB34C6D8E))){
+                Box(modifier = Modifier.fillMaxWidth().fillMaxHeight()){
 
-        lifecycleScope.launch(Dispatchers.IO) { viewModel.playAnimation(binding.infoIcon, 0.0f, 1.0f, 1.0f, LottieDrawable.INFINITE, LottieDrawable.INFINITE) }
+                }
+            }
+        }
     }
 
 
